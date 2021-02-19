8de3f83d6f26809ce460a58d377a565a3afb625e
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-02-19 16:01:18 +0800

    add connect world update source file

---
+++ src/connect_world_update.cc
@@ -0,0 +1,44 @@
+#include "gazebo-plugin-ros/connect_world_update.hh"
+
+GZ_REGISTER_WORLD_PLUGIN(gazebo::connect_world_update);
+
+namespace gazebo
+{
+    connect_world_update::connect_world_update()
+    :   count_(0)
+    {
+    }
+
+    connect_world_update::~connect_world_update()
+    {
+    }
+
+    void connect_world_update::Load(
+        physics::WorldPtr _world,
+        sdf::ElementPtr _sdf
+    )
+    {
+        // Ensure that ROS node for Gazebo has been initialized
+        if(!ros::isInitialized())
+        {
+            int argc = 0; char ** argv = NULL;
+            ros::init(argc, argv, "connect_world_update_node", ros::init_options::NoSigintHandler);
+        }
+
+
+        // Connect to Update Loop
+        this->updateConnection_ = event::Events::ConnectWorldUpdateBegin(
+            [this](...) -> void
+            {
+                /** Do something here, but note that this loop is 10000 Hz
+                 *  Hence, you may want to spin another thread instead of using this update loop.
+                 */
+                ROS_INFO_STREAM(ros::this_node::getName() << " doing something " << this->count_);
+            }
+        );
+
+        // Succesful message for user
+        gzmsg << ros::this_node::getName() << " successfully started connect_world_update plugin." << std::endl;
+    }
+
+} // namespace gazebo
